1459144699aef1ea927346a13897d832deb60a79
Mikhail Shcheglakov <m.shcheglakov@gmail.com>
2019-11-30 21:29:28 +0300

    feat: add utility functions

--- fancy-weather/src/js/main.js
+++ fancy-weather/src/js/main.js
@@ -1,5 +1,66 @@
 import initializeStructure from './initiation';
+import { convertToMeterPerSecond, deleteChildren, setImgQueryString, convertCoords } from './utils';
 
-window.addEventListener('load', () => {
-  initializeStructure();
-});
+initializeStructure();
+
+const TAGS = {
+  buttonBlock: '.button-block',
+  unitsButtons: '.units__btn',
+  searchField: '.search-field',
+  searchForm: '.search-form',
+  submitButton: '.btn__submit',
+  region: '.title__region',
+  date: '.title__date',
+  time: '.title__time',
+  temperature: '.temterature__data',
+  weatherIcon: '.weather-icon',
+  condition: '.condition',
+  feelsLikeData: '.feels-like-data',
+  windData: '.wind-data',
+  humidityData: '.humidity-data',
+  longTermForcast: '.long-time-forecast',
+  forecastCard: '.forecast-card',
+  map: '.map',
+  latitude: '.latitude',
+  longitude: '.longitude',
+};
+
+const NODES = {
+  buttonBlock: document.querySelector(TAGS.buttonBlock),
+  unitsButtons: document.querySelectorAll(TAGS.unitsButtons),
+  searchField: document.querySelector(TAGS.searchField),
+  searchForm: document.querySelector(TAGS.searchForm),
+  submitButton: document.querySelector(TAGS.submitButton),
+  region: document.querySelector(TAGS.region),
+  date: document.querySelector(TAGS.date),
+  time: document.querySelector(TAGS.time),
+  temperature: document.querySelector(TAGS.temperature),
+  weatherIcon: document.querySelector(TAGS.weatherIcon),
+  condition: document.querySelector(TAGS.condition),
+  feelsLikeData: document.querySelector(TAGS.feelsLikeData),
+  windData: document.querySelector(TAGS.windData),
+  humidityData: document.querySelector(TAGS.humidityData),
+  longTermForcast: document.querySelector(TAGS.longTermForcast),
+  map: document.querySelector(TAGS.map),
+  latitude: document.querySelector(TAGS.latitude),
+  longitude: document.querySelector(TAGS.longitude),
+};
+
+const PROPERTIES = {
+  unsplashKey: 'da77f8e93ce7acc3573e17bbcf1419d4faf4ee916d5eaba2720f14d388d62bc9',
+  darkskyKey: '7051a81a7c46a27c23a3f618342a8e28',
+  opencageKey: '1bbe3cf30f934f0ca470af6f7daeccbd',
+  imageUrl: null,
+  location: {
+    latitude: null,
+    longitude: null,
+    name: null,
+  },
+  lang: 'en',
+  units: 'si',
+  map: null,
+  mapPin: null,
+};
+
+// console.log(NODES);
+// console.log(PROPERTIES);
---
+++ fancy-weather/src/js/utils.js
@@ -0,0 +1,39 @@
+function convertToMeterPerSecond(data) {
+  return data * 0.447;
+}
+
+function deleteChildren(root) {
+  while (root.firstChild) {
+    root.removeChild(root.firstChild);
+  }
+}
+
+function setImgQueryString() {
+  const date = new Date();
+  const month = date.getMonth();
+  const hours = date.getHours();
+  let queryString = '';
+
+  if (month <= 10) {
+    queryString += 'autumn';
+  } else if (month <= 7) {
+    queryString += 'summer';
+  } else if (month <= 4) {
+    queryString += 'spring';
+  } else if (month >= 11 && month <= 1) {
+    queryString += 'winter';
+  }
+
+  if (hours > 23 || hours <= 6) {
+    queryString += ',night';
+  }
+
+  return queryString;
+}
+
+function convertCoords(coords) {
+  const dotIdx = coords.indexOf('.');
+  return `${coords.slice(0, dotIdx)}°${coords.slice(dotIdx + 1, dotIdx + 3)}'`;
+}
+
+export { convertToMeterPerSecond, deleteChildren, setImgQueryString, convertCoords };
